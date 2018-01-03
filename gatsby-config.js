8bce00999bfc1fa33fe8b082a291fc57c99789f0
Luke Dodge <luke.r.dodge@gmail.com>
2018-01-03 09:05:58 -0600

    Add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
 module.exports = {
   siteMetadata: {
     title: "Learn Improv Online",
-    siteUrl: `https://www.learnimpovonline.com`
+    siteUrl: "https://www.learnimpovonline.com"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap"
+    "gatsby-plugin-sitemap",    
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Learn Improv Online",
+        short_name: "LIO",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui"
+      },
+    },
+    "gatsby-plugin-offline",
   ],
 }
