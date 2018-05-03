01ab4fd29f15e22a12845b39949e79bfcad58cae
Luke Dodge <luke.r.dodge@gmail.com>
2018-05-03 07:34:49 -0500

    Add focus json

--- content/activity/conventions.js
+++ content/activity/conventions.js
@@ -16,5 +16,5 @@ const conventions = [
     "verbiage": "Form a single, randomized line where the first players in line start the first scene. Players rotate through each scene role until every player has performed a scene as each role. Will result in back-to-back scenes except for the first player who rotates out and will be in the last scene. For example, Player 1 starts as Role B and Player 2 starts as Role A. The next scene will be Player 2 as Role A, Player 3 as Role B, and Player 1 at the back of the line."
   },
 ]
-
+// FIXME: Chnage to json document
 module.exports = conventions
