817891314baad245d5ebd82191bcc321f50ec103
Luke Dodge <luke.r.dodge@gmail.com>
2018-08-20 10:58:43 -0500

    Add search form to navbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import bootstrap from 'bootstrap'
 
 import './_header.scss'
 
 const Header = ({ children }) => (
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container">
       <Link className="navbar-brand" to="/">{children}</Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav mr-auto">
           <Link className="nav-item nav-link" to="/activities">Activities</Link>
           <Link className="nav-item nav-link" to="/faq">FAQ</Link>
           <Link className="nav-item nav-link" to="/contact">Contact</Link>
         </div>
+        <form className="form-inline">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+        </form>
       </div>
     </div>
   </nav>
