dac7df4fcd0f79c90f9327376b8f3e59b5ef1933
Luke Dodge <luke.r.dodge@gmail.com>
2018-12-19 08:04:49 -0600

    Cleanup replace tokens

--- content/activities/replace-tokens.js
+++ content/activities/replace-tokens.js
@@ -1,23 +1,23 @@
-const replace = require('replace-in-file')
-const conventions = require('./conventions.json')
+const replace = require('replace-in-file');
+const conventions = require('./conventions.json');
 
-const from = []
-const to = []
+const from = [];
+const to = [];
 
-conventions.forEach(convention => {
-  const token = new RegExp(`{{ ${convention.name} }}`, 'g')
-  from.push(token)
-  to.push(convention.verbiage)
-})
+conventions.forEach((convention) => {
+  const token = new RegExp(`{{ ${convention.name} }}`, 'g');
+  from.push(token);
+  to.push(convention.verbiage);
+});
 
-console.log(from)
+console.log(from);
 
 const options = {
   files: 'src/data/activities/*.md',
-  from: from,
-  to: to,
-}
+  from,
+  to,
+};
 
-const changes = replace.sync(options)
+const changes = replace.sync(options);
 
-console.log(changes)
+console.log(changes);
