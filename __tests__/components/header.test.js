f730743721d760197ba6c41b182ba122fb42de14
Luke Dodge <luke.r.dodge@gmail.com>
2019-01-15 08:51:01 -0600

    Fix navbar to collapse on navigation

--- __tests__/components/header.test.js
+++ __tests__/components/header.test.js
@@ -1,162 +1,179 @@
 /* eslint-env jest */
-import React from 'react'
-import { shallow } from 'enzyme'
-import Link, * as gatsbyLink from 'gatsby-link'
-import Header, { handleSubmit } from '../../src/components/header'
+import React from 'react';
+import { shallow } from 'enzyme';
+import Link, * as gatsbyLink from 'gatsby-link';
+import Header, { handleSubmit } from '../../src/components/header';
 
-const handleSearchMock = jest.fn()
-const HeaderDom = shallow(<Header handleSearch={handleSearchMock}>{'Site Name'}</Header>)
-jest.mock('gatsby-link')
+const handleSearchMock = jest.fn();
+const HeaderDom = shallow(<Header handleSearch={handleSearchMock}>{'Site Name'}</Header>);
+jest.mock('gatsby-link');
 
 describe('Header', () => {
   describe('handleSubmit', () => {
-    const preventDefaultMock = jest.fn()
-    const resetMock = jest.fn()
-    let navigateToSpy
+    const preventDefaultMock = jest.fn();
+    const resetMock = jest.fn();
+    let navigateToSpy;
     const event = {
       preventDefault: preventDefaultMock,
       target: {
         reset: resetMock,
       },
-    }
+    };
 
     beforeAll(() => {
-      navigateToSpy = jest.spyOn(gatsbyLink, 'navigateTo')
-      handleSubmit(event)
-    })
+      navigateToSpy = jest.spyOn(gatsbyLink, 'navigateTo');
+      handleSubmit(event);
+    });
 
     afterAll(() => {
-      preventDefaultMock.mockClear()
-      navigateToSpy.mockClear()
-    })
+      preventDefaultMock.mockClear();
+      navigateToSpy.mockClear();
+    });
 
     test('prevents form submission', () => {
-      expect(preventDefaultMock).toHaveBeenCalledTimes(1)
-    })
+      expect(preventDefaultMock).toHaveBeenCalledTimes(1);
+    });
 
     test('calls navigateTo with "/search"', () => {
-      expect(navigateToSpy).toHaveBeenCalledWith('/search')
-    })
+      expect(navigateToSpy).toHaveBeenCalledWith('/search');
+    });
 
     test('resets the form', () => {
-      expect(resetMock).toHaveBeenCalledTimes(1)
-    })
-  })
+      expect(resetMock).toHaveBeenCalledTimes(1);
+    });
+  });
   describe('component', () => {
     describe('Site Title', () => {
-      const titleLink = HeaderDom.find('.navbar-brand')
+      const titleLink = HeaderDom.find('.navbar-brand');
 
       test('link goes to the main page', () => {
-        const rootDestination = '/'
+        const rootDestination = '/';
 
-        const linkDestination = titleLink.prop('to')
-        expect(linkDestination).toEqual(rootDestination)
-      })
+        const linkDestination = titleLink.prop('to');
+        expect(linkDestination).toEqual(rootDestination);
+      });
 
       test('display text is passed in as children', () => {
-        const linkText = titleLink.children().text()
+        const linkText = titleLink.children().text();
 
-        expect(linkText).toEqual('Site Name')
-      })
-    })
+        expect(linkText).toEqual('Site Name');
+      });
+    });
 
     describe('Navigation', () => {
-      const navigationItems = HeaderDom.find('.nav-item')
+      const navigationItems = HeaderDom.find('.nav-item');
 
-      test('should have three navigation items', () => {
-        expect(navigationItems).toHaveLength(3)
-      })
+      test('collapses navbar on navigation', () => {
+        const navigationItemsContainer = HeaderDom.find('.navbar-nav');
+        const dataToggle = navigationItemsContainer.prop('data-toggle');
+        const dataTarget = navigationItemsContainer.prop('data-target');
+
+        expect(dataToggle).toBe('collapse');
+        expect(dataTarget).toBe('#navbarNavAltMarkup');
+      });
+
+      test('has three navigation items', () => {
+        expect(navigationItems).toHaveLength(3);
+      });
 
       describe('Activities', () => {
-        const activitiesLink = navigationItems.at(0)
+        const activitiesLink = navigationItems.at(0);
 
         test('link goes to Activities page', () => {
-          const activitesPageDestination = '/activities'
-          const linkDestination = activitiesLink.prop('to')
+          const activitesPageDestination = '/activities';
+          const linkDestination = activitiesLink.prop('to');
 
-          expect(activitiesLink.type()).toEqual(Link)
-          expect(linkDestination).toEqual(activitesPageDestination)
-        })
+          expect(activitiesLink.type()).toEqual(Link);
+          expect(linkDestination).toEqual(activitesPageDestination);
+        });
         test('display text is "Activities"', () => {
-          const linkText = activitiesLink.children().text()
+          const linkText = activitiesLink.children().text();
 
-          expect(linkText).toEqual('Activities')
-        })
-      })
+          expect(linkText).toEqual('Activities');
+        });
+      });
 
       describe('FAQ', () => {
-        const faqLink = navigationItems.at(1)
+        const faqLink = navigationItems.at(1);
 
         test('link goes to FAQ page', () => {
-          const faqPageDestination = '/faq'
-          const linkDestination = faqLink.prop('to')
+          const faqPageDestination = '/faq';
+          const linkDestination = faqLink.prop('to');
 
-          expect(faqLink.type()).toEqual(Link)
-          expect(linkDestination).toEqual(faqPageDestination)
-        })
+          expect(faqLink.type()).toEqual(Link);
+          expect(linkDestination).toEqual(faqPageDestination);
+        });
         test('display text is "FAQ"', () => {
-          const linkText = faqLink.children().text()
+          const linkText = faqLink.children().text();
 
-          expect(linkText).toEqual('FAQ')
-        })
-      })
+          expect(linkText).toEqual('FAQ');
+        });
+      });
 
       describe('Contact', () => {
-        const contactLink = navigationItems.at(2)
+        const contactLink = navigationItems.at(2);
 
         test('link goes to Contact page', () => {
-          const contactPageDestination = '/contact'
-          const linkDestination = contactLink.prop('to')
+          const contactPageDestination = '/contact';
+          const linkDestination = contactLink.prop('to');
 
-          expect(contactLink.type()).toEqual(Link)
-          expect(linkDestination).toEqual(contactPageDestination)
-        })
+          expect(contactLink.type()).toEqual(Link);
+          expect(linkDestination).toEqual(contactPageDestination);
+        });
         test('display text is "Contact"', () => {
-          const linkText = contactLink.children().text()
+          const linkText = contactLink.children().text();
 
-          expect(linkText).toEqual('Contact')
-        })
-      })
-    })
+          expect(linkText).toEqual('Contact');
+        });
+      });
+    });
 
     describe('Search Form', () => {
-      const searchForm = HeaderDom.find('form')
+      const searchForm = HeaderDom.find('form');
 
       test('exists', () => {
-        expect(searchForm).toHaveLength(1)
-      })
+        expect(searchForm).toHaveLength(1);
+      });
 
       test('has onSubmit set to handleSubmit', () => {
-        expect(searchForm.props().onSubmit).toBe(handleSubmit)
-      })
+        expect(searchForm.props().onSubmit).toBe(handleSubmit);
+      });
 
       describe('Input Field', () => {
-        const searchField = searchForm.find('input')
+        const searchField = searchForm.find('input');
 
         test('is type of Search', () => {
-          expect(searchField.props().type).toBe('search')
-        })
+          expect(searchField.props().type).toBe('search');
+        });
 
         test('has onChange passed from props', () => {
-          expect(searchField.props().onChange).toBe(handleSearchMock)
-        })
-      })
+          expect(searchField.props().onChange).toBe(handleSearchMock);
+        });
+      });
 
       describe('Search Button', () => {
-        const searchButton = searchForm.find('button')
+        const searchButton = searchForm.find('button');
 
         test('exists', () => {
-          expect(searchButton).toHaveLength(1)
-        })
+          expect(searchButton).toHaveLength(1);
+        });
 
         test('is type "submit"', () => {
-          expect(searchButton.props().type).toBe('submit')
-        })
+          expect(searchButton.props().type).toBe('submit');
+        });
 
         test('has display text of "Search"', () => {
-          expect(searchButton.children().text()).toBe('Search')
-        })
-      })
-    })
-  })
-})
+          expect(searchButton.children().text()).toBe('Search');
+        });
+
+        test('collapses navbar on click', () => {
+          const dataToggle = searchButton.prop('data-toggle');
+          const dataTarget = searchButton.prop('data-target');
+
+          expect(dataToggle).toBe('collapse');
+          expect(dataTarget).toBe('#navbarNavAltMarkup');
+        });
+      });
+    });
+  });
+});
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,26 +1,46 @@
-import React from 'react'
-import Link, { navigateTo } from 'gatsby-link'
+import React from 'react';
+import Link, { navigateTo } from 'gatsby-link';
 // eslint-disable-next-line no-unused-vars
-import bootstrap from 'bootstrap'
+import bootstrap from 'bootstrap';
 
 export const handleSubmit = (event) => {
-  event.preventDefault()
-  navigateTo('/search')
-  event.target.reset()
-}
+  event.preventDefault();
+  navigateTo('/search');
+  event.target.reset();
+};
 
 const Header = ({ children, handleSearch }) => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container">
-      <Link className="navbar-brand" to="/">{children}</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <Link className="navbar-brand" to="/">
+        {children}
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav mr-auto">
-          <Link className="nav-item nav-link" to="/activities">Activities</Link>
-          <Link className="nav-item nav-link" to="/faq">FAQ</Link>
-          <Link className="nav-item nav-link" to="/contact">Contact</Link>
+        <div
+          className="navbar-nav mr-auto"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+        >
+          <Link className="nav-item nav-link" to="/activities">
+            Activities
+          </Link>
+          <Link className="nav-item nav-link" to="/faq">
+            FAQ
+          </Link>
+          <Link className="nav-item nav-link" to="/contact">
+            Contact
+          </Link>
         </div>
         <form onSubmit={handleSubmit} className="form-inline">
           <input
@@ -30,11 +50,18 @@ const Header = ({ children, handleSearch }) => (
             placeholder="Search"
             aria-label="Search"
           />
-          <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+          <button
+            className="btn btn-outline-primary my-2 my-sm-0"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            type="submit"
+          >
+            Search
+          </button>
         </form>
       </div>
     </div>
   </nav>
-)
+);
 
-export default Header
+export default Header;
